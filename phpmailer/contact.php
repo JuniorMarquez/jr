f46c66bf589854bd737eb6ba7f94308e1bb88d9f
Junior Marquez <junior.marquez.hsc@gmail.com>
2019-01-12 14:45:58 +0000

    phpmailer

--- phpmailer/contact.php
+++ phpmailer/contact.php
@@ -6,7 +6,7 @@ $mail = new PHPMailer;
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = $_POST['managerAccountHost'];      //  ssl://smtp.gmail.com          // Specify main and backup SMTP servers
+$mail->Host = 'thetixs.com';       //  ssl://smtp.gmail.com          // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'developer@thetixs.com';                      // SMTP username
 $mail->Password = 'uoW9developer';                           // SMTP password
@@ -15,9 +15,9 @@ $mail->Port = 465;          //587                          // TCP port to connec
 
 
 //echo $email= $_POST['n2']."@".$_POST['n3'];
-$mail->setFrom('developer@thetixs.com', 'juniormarquez.com.ve');
+$mail->setFrom('developer@thetixs.com', 'AndreaZambrano.com.ve');
 //$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-$mail->addAddress('junior.marquez.hsc@gmail.com');              // Name is optional
+$mail->addAddress('andreazambrano10@gmail.com');              // Name is optional
 //$mail->addReplyTo('$email','roshan');
 //$mail->addCC('passmethecode@gmail.com');
 //$mail->addBCC('bcc@example.com');
@@ -26,7 +26,7 @@ $mail->addAddress('junior.marquez.hsc@gmail.com');              // Name is optio
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Contacto CV Junior Marquez';
+$mail->Subject = 'Contacto CV AndreaZambrano';
 $mail->Body    = 
    
     '<div align="center" style="background-color:#f6f8f8;">
@@ -35,23 +35,26 @@ $mail->Body    =
                                   width:100%;">
           
 
-          <div style="; padding:20px;" align="left">
+          <div style="color:#e44c2a; padding:20px;" align="left">
             <br/>
-            <b>Nombre: </b>
-           <b>'.$_POST['name'].'</b>  
+           <b>'.$_POST['name'].'</b>, 
+                    
+          </div>
+          
+          <div style="color:#e44c2a; padding:20px;" align="left">
+            <br/>
+           <b>'.$_POST['email'].'</b>, 
                     
           </div>
 
-           <div style=" padding:20px;" align="left">
+          <div style="color:#e44c2a; padding:20px;" align="left">
             <br/>
-            <b>Email: </b>
-           '.$_POST['email'].' , <b>Telefono: </b>  '.$_POST['phone'].'        
+           <b>'.$_POST['phone'].'</b>,                     
           </div>
 
-          <div style=" padding:20px;" align="left">
+          <div style="color:#e44c2a; padding:20px;" align="left">
             <br/>
-            <b>Mensaje: </b>
-           '.$_POST['message'].'                     
+           <b>'.$_POST['message'].'</b>,                     
           </div>
 
           <div style="border-radius:10px; width: 320PX; box-shadow:5px 5px 17px 0px rgba(44, 50, 50, 0.14);">
